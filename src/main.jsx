d29b7e10a82b560c65decbc71cfc00f755f00f57
A2D-mohd-mizna-ansari <mohd.ansari@a2d.co.in>
2025-07-26 02:52:57 +0530

    adding pwa

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,10 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from "react-router";
+// Register the service worker
+import { registerSW } from 'virtual:pwa-register'
 
+registerSW({ immediate: true }) // or omit options
 createRoot(document.getElementById('root')).render(
    <BrowserRouter>
     <App />
