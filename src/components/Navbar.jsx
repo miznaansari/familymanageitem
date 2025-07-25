273cd62a26e7cc377135775ca9a96c6444827227
A2D-mohd-mizna-ansari <mohd.ansari@a2d.co.in>
2025-07-26 02:26:28 +0530

    add button

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -189,6 +189,7 @@ const Navbar = () => {
                     >
                         <li><Link to="/">Home</Link></li>
                         <li><Link to="/signup">Signup</Link></li>
+                        <li><Link to="/add">Add</Link></li>
                         {!ableNotification && (
                             <li>
                                 <button onClick={handleAllowNotification}>
